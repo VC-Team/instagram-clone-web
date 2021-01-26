4351864a0304bdf90f6cac57ab9440ac4fb2822d
huynqalta <huynq@alta.com.vn>
2021-01-26 17:15:13 +0700

    add login

---
+++ src/ui/view/atoms/Input/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import classnames from "classnames"
+import classes from './style.module.scss';
+export interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+    className?: string;
+    disabled?: boolean;
+    type?: "button" | "checkbox" | "color" | "date" | "datetime-local" | "email" | "file" | "hidden" | "image" | "month" | "number" | "password" | "radio" | "reset" | "search" | "submit" | "tel" | "text" | "time" | "url" | "week";
+    value?: string;
+    id?: string;
+    readonly?: boolean;
+    size?: number;
+    maxlength?: number;
+    min?: string | number;
+    max?: string | number;
+    multiple?: boolean;
+    pattern?: string;
+    placeholder?: string;
+    required?: boolean;
+    step?: string | number;
+    autofocus?: boolean;
+}
+
+const Input: React.FC<InputProps> = (props) => {
+    const { className, type = "text", ...inputProps } = props;
+    return (
+        <input className={classnames(className)} {...inputProps} type={type} />
+    )
+}
+
+export default Input
