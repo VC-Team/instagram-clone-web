23ef16b458a97e350290b22f69a5044396b4a0c4
DevPhuc <phucdinhnguyen09052000@gmail.com>
2021-01-27 21:10:45 +0700

    update useNotification

--- src/ui/viewModels/useNotification.ts
+++ src/ui/viewModels/useNotification.ts
@@ -6,8 +6,27 @@ const { getListNotification, subcribeNotification } = new NotificationPresenter(
 
 const useNotification = (listNotification: Array<NotificationEntity>) => {
 
+    const getContentByType = (notif: NotificationEntity) => {
+        switch (notif.type) {
+            case 1:
+                return notif.actionContent.content
+            case 2:
+                return notif.actionContent.content
+
+            default:
+                return 'like'
+        }
+    }
+
+    const convertListNotification = (listNotif) => {
+        listNotif.map(notif => ({
+            ...notif,
+            content: getContentByType(notif)
+        }))
+    }
+
     return {
-        listNotification,
+        listNotification: convertListNotification(listNotification),
         getListNotification: useAsync(getListNotification)[0],
         subcribeNotification
     }
